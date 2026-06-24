3189cccb3efddd77233405c45005394cdcbaf14e
JoonOh-Lee <77381505+JoonOh-Lee@users.noreply.github.com>
2026-06-24 09:52:28 +0900

    data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@
 INSERT INTO station (name, sort_order, is_active, created_at, updated_at) VALUES
                                                                               ('아부리다이', 0, true, NOW(), NOW()),
                                                                               ('육회다이', 1, true, NOW(), NOW()),
-                                                                              ('연어다이', 2, true, NOW(), NOW()),
+                                                                              ('참치다이', 2, true, NOW(), NOW()),
                                                                               ('활어다이', 3, true, NOW(), NOW()),
                                                                               ('뒷주방', 4, true, NOW(), NOW());
 
